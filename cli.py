e74553ac65e9f19d406682856cf0b6f1ab1c1ff0
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2022-02-09 15:32:55 +0100

    Add progress bar to database initializer (#10)
    * Add example queries
    
    * Add progress bar to database initializer
    
    * Resolve merge conflicts
    
    * Re-order imports
    
    * Remove unused lines

--- cli.py
+++ cli.py
@@ -1,15 +1,14 @@
-from email.policy import default
-from rich import print
 import click
 import pandas as pd
 import sqlite3
+from rich import print
+from tqdm import tqdm
+from queries import get_user_data
 
 from queries import get_user_data
 
 database = "data.db"
 tables = ["typed_posts", "typed_comments", "mbti9k_comments"]
-connection = sqlite3.connect(database)
-cursor = connection.cursor()
 
 
 @click.group()
@@ -25,21 +24,49 @@ def hello(count, name):
         print(f"Hello [bold red]{name}[/bold red]!")
 
 
-def load_dataset_to_sql(dataset="typed_posts", folder="./data"):
-    path = f"{folder}/{dataset}.csv"
+def chunker(seq, size):
+    # from http://stackoverflow.com/a/434328
+    return (seq[pos : pos + size] for pos in range(0, len(seq), size))
 
-    print(f"Loading dataset: {path}")
-    df = pd.read_csv(path)
 
-    print(f"Writing dataset to table {dataset} in {database}")
-    df.to_sql(dataset, connection, if_exists="replace", index=False)
-    print()
+def insert_with_progress(df, table: str):
+    # from https://stackoverflow.com/a/39495229
+    connection = sqlite3.connect(database)
+    chunksize = int(len(df) / 100)  # 1%
+    with tqdm(total=len(df)) as pbar:
+        for i, cdf in enumerate(chunker(df, chunksize)):
+            replace = "replace" if i == 0 else "append"
+            cdf.to_sql(con=connection, name=table, if_exists=replace, index=False)
+            pbar.update(chunksize)
+    connection.close()
+
+
+def drop_table(table: str):
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+    query = f"""
+    --sql
+    DROP TABLE IF EXISTS {table}
+    ;
+    """
+    cursor.execute(query)
+    connection.close()
 
 
 @cli.command()
 def init():
+    folder = "./data"
     for table in tables:
-        load_dataset_to_sql(dataset=table)
+        path = f"{folder}/{table}.csv"
+        print(f"Loading dataset: {path}")
+        df = pd.read_csv(path)
+
+        drop_table(table)
+        print(f"Writing dataset to table {table} in {database}")
+
+        insert_with_progress(df, table)
+        print()
+
     print(f"Successfully loaded {len(tables)} files into SQLite")
 
 
@@ -47,6 +74,8 @@ def init():
 @click.option("--count", default=2, help="Number of rows to fetch")
 @click.option("--table", type=click.Choice(tables), help="Table to sample")
 def sample(table, count):
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
     query = f"""
     --sql
     SELECT * FROM {table}
@@ -67,6 +96,7 @@ def sample(table, count):
 )
 @click.option("--user", help="User to fetch data about")
 def get_user(table, user):
+    connection = sqlite3.connect(database)
     rows = get_user_data(connection, table, user)
     for row in rows:
         print(row)
