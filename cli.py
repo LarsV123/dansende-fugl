ba5782bf2e203f8cd1f1aa22f3886560da7f90f1
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2022-02-09 11:42:42 +0100

    Add CLI to handle database initialization

---
+++ cli.py
@@ -0,0 +1,59 @@
+from rich import print
+import click
+import pandas as pd
+import sqlite3
+
+database = "data.db"
+tables = ["typed_posts", "typed_comments", "mbti9k_comments"]
+connection = sqlite3.connect(database)
+cursor = connection.cursor()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--count", default=1, help="Number of greetings")
+@click.argument("name")
+def hello(count, name):
+    for _ in range(count):
+        print(f"Hello [bold red]{name}[/bold red]!")
+
+
+def load_dataset_to_sql(dataset="typed_posts", folder="./data"):
+    path = f"{folder}/{dataset}.csv"
+
+    print(f"Loading dataset: {path}")
+    df = pd.read_csv(path)
+
+    print(f"Writing dataset to table {dataset} in {database}")
+    df.to_sql(dataset, connection, if_exists="replace", index=False)
+    print()
+
+
+@cli.command()
+def init():
+    for table in tables:
+        load_dataset_to_sql(dataset=table)
+    print(f"Successfully loaded {len(tables)} files into SQLite")
+
+
+@cli.command()
+@click.option("--count", default=2, help="Number of rows to fetch")
+@click.option("--table", type=click.Choice(tables), help="Table to sample")
+def sample(table, count):
+    query = f"""
+    --sql
+    SELECT * FROM {table}
+    LIMIT {count}
+    ;
+    """
+    rows = cursor.execute(query).fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    cli()
