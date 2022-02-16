6a9e9d5a7e4bd7798bccb523a58387833bbe159d
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2022-02-16 10:53:51 +0100

    Give indexes unique names

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,15 +1,15 @@
-CREATE INDEX author ON typed_comments(author);
-CREATE INDEX subreddit_id ON typed_comments(subreddit_id);
-CREATE INDEX subreddit ON typed_comments(subreddit);
-CREATE INDEX lang ON typed_comments(lang);
-CREATE INDEX created_at ON typed_comments(created_utc);
+CREATE INDEX comments_author ON typed_comments(author);
+CREATE INDEX comments_subreddit_id ON typed_comments(subreddit_id);
+CREATE INDEX comments_subreddit ON typed_comments(subreddit);
+CREATE INDEX comments_lang ON typed_comments(lang);
+CREATE INDEX comments_created_at ON typed_comments(created_utc);
 
-CREATE INDEX author ON typed_posts(author);
-CREATE INDEX subreddit_id ON typed_posts(subreddit_id);
-CREATE INDEX subreddit ON typed_posts(subreddit);
-CREATE INDEX mbti_type ON typed_posts(type);
-CREATE INDEX created_at ON typed_posts(created_utc);
+CREATE INDEX posts_author ON typed_posts(author);
+CREATE INDEX posts_subreddit_id ON typed_posts(subreddit_id);
+CREATE INDEX posts_subreddit ON typed_posts(subreddit);
+CREATE INDEX posts_mbti_type ON typed_posts(type);
+CREATE INDEX posts_created_at ON typed_posts(created_utc);
 
-CREATE INDEX author ON mbti9k_comments(author);
-CREATE INDEX mbti_type ON mbti9k_comments(type);
-CREATE INDEX comments_num ON mbti9k_comments(comments_num);
+CREATE INDEX mbti9k_author ON mbti9k_comments(author);
+CREATE INDEX mbti9k_type ON mbti9k_comments(type);
+CREATE INDEX mbti9k_comments_num ON mbti9k_comments(comments_num);
