535750acc987a3f75ddbc2e2abee523d61787edb
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2022-04-06 08:57:24 +0200

    Remove duplicate SQL

--- schema/summary_tables.sql
+++ schema/summary_tables.sql
@@ -29,34 +29,6 @@ SELECT mbti,
   COUNT(*) AS mbti_post_count
 FROM posts
 GROUP BY mbti;
--- Comments per hour per type
-DROP TABLE IF EXISTS comments_per_hour;
-CREATE TEMP TABLE total_comments_per_mbti AS (
-  SELECT mbti,
-    COUNT(*) AS total_comments
-  FROM unique_comments
-  GROUP BY mbti
-);
-CREATE TEMP TABLE temp_comments_per_hour AS (
-  SELECT mbti,
-    EXTRACT(
-      HOUR
-      FROM to_timestamp(created_utc)
-    ) AS hour,
-    COUNT(*) AS comments_per_hour
-  FROM unique_comments
-  GROUP BY mbti,
-    hour
-);
-CREATE TABLE comments_per_hour AS (
-  SELECT mbti,
-    comments_per_hour,
-    total_comments,
-    hour,
-    comments_per_hour * 1.0 / total_comments * 1.0 AS share
-  FROM total_comments_per_mbti AS a
-    NATURAL JOIN temp_comments_per_hour
-);
 DROP TABLE IF EXISTS top_subreddits_mbti;
 CREATE TABLE top_subreddits_mbti AS (
   SELECT subreddit,
