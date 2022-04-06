b24d5fb5af67a61fde5b9aa9339776b919ccf6ca
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2022-04-06 08:32:25 +0200

    Clean up SQL from day 12
    Remove unused summary tables and change
    existing SQL to use the unique_comments table.

--- schema/summary_tables.sql
+++ schema/summary_tables.sql
@@ -5,9 +5,9 @@ SELECT mbti,
   COUNT(*) AS mbti_comment_count,
   COUNT(*) * 1.0 / (
     SELECT COUNT(*)
-    FROM comments
+    FROM unique_comments
   ) AS share
-FROM comments
+FROM unique_comments
 GROUP BY mbti
 ORDER BY mbti DESC;
 -- Unique version
@@ -34,7 +34,7 @@ DROP TABLE IF EXISTS comments_per_hour;
 CREATE TEMP TABLE total_comments_per_mbti AS (
   SELECT mbti,
     COUNT(*) AS total_comments
-  FROM comments
+  FROM unique_comments
   GROUP BY mbti
 );
 CREATE TEMP TABLE temp_comments_per_hour AS (
@@ -44,7 +44,7 @@ CREATE TEMP TABLE temp_comments_per_hour AS (
       FROM to_timestamp(created_utc)
     ) AS hour,
     COUNT(*) AS comments_per_hour
-  FROM comments
+  FROM unique_comments
   GROUP BY mbti,
     hour
 );
@@ -61,7 +61,7 @@ DROP TABLE IF EXISTS top_subreddits_mbti;
 CREATE TABLE top_subreddits_mbti AS (
   SELECT subreddit,
     COUNT(*) AS comment_count
-  FROM comments
+  FROM unique_comments
   GROUP BY subreddit
   ORDER BY comment_count DESC
   LIMIT 25
@@ -70,7 +70,17 @@ DROP TABLE IF EXISTS top_subreddits CASCADE;
 CREATE TABLE top_subreddits AS (
   SELECT subreddit,
     COUNT(*) AS comment_count
-  FROM comments
+  FROM unique_comments
+  WHERE NOT is_mbti_related
+  GROUP BY subreddit
+  ORDER BY comment_count DESC
+  LIMIT 25
+);
+DROP TABLE IF EXISTS top_unique_subreddits CASCADE;
+CREATE TABLE top_unique_subreddits AS (
+  SELECT subreddit,
+    COUNT(*) AS comment_count
+  FROM unique_comments
   WHERE NOT is_mbti_related
   GROUP BY subreddit
   ORDER BY comment_count DESC
@@ -84,7 +94,7 @@ CREATE VIEW top_comments AS (
       SELECT subreddit,
         mbti,
         COUNT(*) AS mbti_comments
-      FROM comments
+      FROM unique_comments
       WHERE subreddit IN (
           SELECT subreddit
           FROM top_subreddits
@@ -94,17 +104,54 @@ CREATE VIEW top_comments AS (
     ) AS foo
   ORDER BY comment_count DESC
 );
-DROP VIEW IF EXISTS heatmap;
-CREATE VIEW heatmap AS (
-  SELECT *
-  FROM top_subreddits
-    NATURAL JOIN (
-      SELECT subreddit,
-        mbti,
-        COUNT(*) AS mbti_comments
-      FROM comments
-      GROUP BY subreddit,
-        mbti
-    ) AS foo
-  ORDER BY comment_count DESC
-);
+-- Comment count per subreddit and MBTI type
+DROP TABLE IF EXISTS mbti_per_subreddit;
+CREATE TABLE mbti_per_subreddit AS
+SELECT subreddit,
+  mbti,
+  COUNT(*) AS comment_count
+FROM unique_comments
+WHERE subreddit IN (
+    SELECT subreddit
+    FROM top_unique_subreddits
+  )
+GROUP BY subreddit,
+  mbti;
+-- Comment count per user and subreddit
+DROP TABLE IF EXISTS user_comments_per_subreddit;
+CREATE TABLE user_comments_per_subreddit AS
+SELECT author,
+  subreddit,
+  mbti,
+  COUNT(*) AS comment_count_by_user
+FROM unique_comments
+WHERE subreddit IN (
+    SELECT subreddit
+    FROM top_unique_subreddits
+  )
+GROUP BY author,
+  subreddit,
+  mbti;
+-- User statistics per subreddit
+DROP TABLE IF EXISTS users_per_subreddit;
+CREATE TABLE users_per_subreddit AS
+SELECT subreddit,
+  mbti,
+  MAX(comment_count_by_user) AS max_user_comment_count,
+  PERCENTILE_DISC(0.5) WITHIN GROUP (
+    ORDER BY comment_count_by_user
+  ) AS median_comment_count,
+  PERCENTILE_DISC(0.9) WITHIN GROUP (
+    ORDER BY comment_count_by_user
+  ) AS percentile_90_comment_count,
+  PERCENTILE_DISC(0.95) WITHIN GROUP (
+    ORDER BY comment_count_by_user
+  ) AS percentile_95_comment_count,
+  ROUND(AVG(comment_count_by_user)) AS average_comment_count,
+  SUM(comment_count_by_user) AS total_comment_count,
+  COUNT(*) AS distinct_users
+FROM user_comments_per_subreddit
+GROUP BY subreddit,
+  mbti
+ORDER BY subreddit,
+  mbti;
