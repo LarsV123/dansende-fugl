e3e86011e3eea9b7c3f51a5fdb4a8c52ed19f0c8
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2022-05-02 16:36:35 +0200

    Add relative interest query for subreddit groups

--- schema/relative_interest.sql
+++ schema/relative_interest.sql
@@ -90,3 +90,42 @@ CREATE TABLE relative_interest_distinct_users AS (
   SELECT *
   FROM x_f
 );
+DROP TABLE IF EXISTS relative_interest_grouped;
+CREATE TABLE relative_interest_grouped AS (
+  WITH distinct_users AS (
+    SELECT DISTINCT author,
+      mbti,
+      g.group_name AS subreddit_group
+    FROM unique_comments AS c
+      INNER JOIN subreddit_groups AS g ON c.subreddit = g.subreddit
+    WHERE NOT is_mbti_related
+  ),
+  x AS (
+    SELECT mbti,
+      subreddit_group,
+      COUNT(*) AS users_per_subreddit_by_mbti
+    FROM distinct_users
+    GROUP BY mbti,
+      subreddit_group
+  ),
+  x_tot AS (
+    SELECT mbti,
+      SUM(users_per_subreddit_by_mbti) AS total_users_by_mbti
+    FROM x
+    GROUP BY mbti
+  ),
+  x_f AS (
+    SELECT x.mbti,
+      x.subreddit_group,
+      total_users_by_mbti,
+      users_per_subreddit_by_mbti,
+      ROUND(
+        users_per_subreddit_by_mbti * 1.0 / total_users_by_mbti * 1.0,
+        4
+      ) AS share
+    FROM x
+      INNER JOIN x_tot ON x.mbti = x_tot.mbti
+  )
+  SELECT *
+  FROM x_f
+);
